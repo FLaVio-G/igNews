b5b68e41f1c19e4b8610b747fb814dafed3b33fb
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-04-06 22:43:46 -0300

    Alterações simples

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,8 @@
+import {FaGithub} from 'react-icons/fa'
 export function SignInButton() {
     return (
         <button type="button">
+            <FaGithub/>
 Sign in with Github
         </button>
     )
