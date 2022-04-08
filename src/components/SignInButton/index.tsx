9fe05864bdecffb1534ca525642fad05719af70d
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-04-08 18:32:49 -0300

    Alterações no componente signInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,22 @@
-import {FaGithub} from 'react-icons/fa'
-export function SignInButton() {
-    return (
-        <button type="button">
-            <FaGithub/>
-Sign in with Github
-        </button>
-    )
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 
+
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Flávio Viana
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
 }
