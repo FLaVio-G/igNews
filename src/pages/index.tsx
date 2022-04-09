ddb93e45e1eb00f8c808b6622d227b02b8d3040c
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-04-09 15:58:02 -0300

    Alterações simples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -8,18 +9,22 @@ export default function Home() {
       <Head>
         <title>Home | IgNews</title>
       </Head>
-     <main className={styles.contentContainer}>
-       <section className={styles.hero}>
-      <span>👏 Hey, welcome</span>
-      <h1> News about the <span>React</span>Wordl</h1>
-      <p>
-        Get acess to all the publications <br/>
-        <span>for  $9.90</span>
-      </p>
-       </section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            {" "}
+            News about the <span>React</span>Wordl
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+          <SubscribeButton />
+        </section>
 
-       <img src="/images/avatar.svg" alt="Girl coding" />
-     </main>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
