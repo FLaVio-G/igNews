bf94f8254f89b60b9036ddd5a2312e89152bfddf
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-04-11 15:52:08 -0300

    Implementação do Static  Site Generation(SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -39,7 +39,7 @@ export default function Home({product}: HomeProps) {
   );
 }
 
-export const  getServerSideProps: GetServerSideProps = async () => {
+export const  getStaticProps: GetStaticProps = async () => {
 const price = await stripe.prices.retrieve('price_1Kmk8jAK85FCE6K35Ft4DVEX')
 const product = {
   priceId: price.id,
@@ -53,6 +53,7 @@ const product = {
   return { 
     props: {
       product,
-  }
+  },
+  revalidate: 60 * 60 * 24, //24horas
   }
 }
