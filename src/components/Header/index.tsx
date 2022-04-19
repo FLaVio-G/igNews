4b6785e055579f8166683d6ee0338dbe2da700f4
KaTSu113 <flaviogabrielandrade@gmail.com>
2022-04-19 13:47:03 -0300

    Implementação da navegação do menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,25 @@
-import { SignInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { useRouter } from  'next/router'
+import Link from "next/link"
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
-                <SignInButton/>
-            </div>
-        </header>
-    )
+    const {asPath} = useRouter()
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <Link href="/">
+            <a className={asPath == '/' ? styles.active : ''}>Home</a>
+          </Link>
+
+          <Link href="/posts" prefetch>
+            <a className={asPath == '/posts' ? styles.active : ''}>Posts</a>
+          </Link>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
